63b48dc2005133477b719ddf037070ced487db06
Unknown <victorperson10@hotmail.fr>
2018-02-11 22:43:52 +0100

    Doxygen template for generation

--- parseur_sources/generation_clustered.h
+++ parseur_sources/generation_clustered.h
@@ -5,24 +5,48 @@
 #include "cluster.h"
 #include <vector>
 
+/*!
+ * \brief Generation class : represents a generation
+ */
 class Generation_clustered
 {
     private :
-        std::vector<Cluster> _cluster;
+        std::vector<Cluster> _cluster; /*! A vector representing all clusters inside the generation*/
     public :
         //Constructor
+        //! Default constructor of a generation. Set the number of individual and number_cluster to -1*/
+        /*!
+         * \param number_individual : an int representing the number of individual inside a generation
+         * \param number_cluster : an int representing the number of cluster inside a generation
+         */
         Generation_clustered(int number_individual = -1, int number_cluster = -1);
+
+        //! Copy constructor
+        /*!
+         * Creates a new generation and copy every members of the old one
+         */
         Generation_clustered(const Generation_clustered&) ;
 
         //Destructor
+        //! Default destructor*/
+        /*!
+          * Destroy the generation and free every cluster inside it
+          */
         ~Generation_clustered();
 
-        //Getter
-        std::vector<Cluster> getCluster() const ;
-
         //operator
+        //! Vector-like operator for the generation*/
         Cluster & operator [] (int i);
+
+        //! Vector-like operator for the generation*/
         Cluster operator [] (int i) const;
+
+        //Getter
+        //! Get the vector of cluster of the generation*/
+        /*!
+         * \return the vector of all cluster inside the generation
+         */
+        std::vector<Cluster> getCluster() const ;
 };
 
 
